f4408032e341d58d59656c3c68977006d52e0591
Ritik <ritikpal227@gmail.com>
2026-01-22 23:59:22 +0530

    Updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-  const [user, setUser] = useState("User");
+  const [user, setUser] = useState("Admin");
   return (
     <>
       <nav
